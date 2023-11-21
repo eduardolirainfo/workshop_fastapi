74a05a2a2013fa26b4c01d06df03ca67314195ef
Eduardo Lira <eduardolirainfo@gmail.com>
2023-11-21 19:14:22 -0300

    autenticação e posts

--- pamps/models/user.py
+++ pamps/models/user.py
@@ -1,7 +1,10 @@
-from typing import Optional
-from sqlmodel import SQLModel, Field
+from typing import Optional, TYPE_CHECKING, List
+from sqlmodel import SQLModel, Field, Relationship
 from pamps.security import HashedPassword
 from pydantic import BaseModel
+
+if TYPE_CHECKING:
+    from pamps.models.post import Post
 
 
 class User(SQLModel, table=True):
@@ -13,6 +16,9 @@ class User(SQLModel, table=True):
     avatar: Optional[str] = None
     bio: Optional[str] = None
     password: HashedPassword
+
+    # it populates the .user attribute on the Content Model
+    posts: List["Post"] = Relationship(back_populates="user")
 
 
 class UserResponse(BaseModel):
